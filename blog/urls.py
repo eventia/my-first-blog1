df25650613d8af8aaf0e1387f3dc04eaebc6ccc4
eventia <eventia@gmail.com>
2017-11-23 22:36:27 +0900

    modified templates to display posts from database

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^branch1/$', views.branch1, name='branch1')
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
+    return render(request, 'blog/post_list.html', {'posts':posts})
+
+def branch1(request):
+    return render(request, 'blog/branch1.html', {})
