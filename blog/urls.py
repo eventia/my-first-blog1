b1c0000c83b370b01562f62c4a863dc65849ae3a
eventia <eventia@gmail.com>
2017-11-24 09:06:21 +0900

    Added views and templates for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^branch1/$', views.branch1, name='branch1')
+    url(r'^branch1/$', views.branch1, name='branch1'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 
 ]
