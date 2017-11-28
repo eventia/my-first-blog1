a4d0d20a233ff126e0eedb91401ceac58618fb59
eventia <eventia@gmail.com>
2017-11-29 06:10:08 +0900

    Log in & out

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Post    # 새로 만든 Post 클래스 추가
+from .models import Post, Comment    # 새로 만든 Post 클래스 추가
 
 admin.site.register(Post)
+admin.site.register(Comment)
